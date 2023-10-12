7a240068e08dd6fb1339218633e382f112995ea8
Prajwal Belur <92054352+PrajwalBelur@users.noreply.github.com>
2023-10-12 19:38:21 +0530

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,170 @@
+const logotext = "Prajwal's portfolio";
+const meta = {
+    title: "Prajwal Belur",
+    description: "I’m Prajwal Belur Solution Architect _ Cloud Adminstrator, currently looking up for oportunities",
+};
+
+const introdata = {
+    title: "I’m Prajwal Belur",
+    animated: {
+        first: "Linux | AWS | DevOps",
+        second: "I code front-ends",
+        third: "Ethical Hacking, Digital Forensics",
+    },
+    description: "Post-Graduation in Master of Computer Applications",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "I am a recent graduate with a Master's degree in Computer Applications, eager to embark on a promising career in the field of technology. As a fresher, I bring a strong foundation in computer science, along with a passion for learning and staying updated with the latest advancements. I am enthusiastic about applying my knowledge and skills to contribute to innovative projects, solve complex problems, and grow as a professional in the dynamic world of computer applications.",
+};
+const worktimeline = [{
+        jobtitle: "Internship",
+        where: "AgileTech, Mangaluru",
+        date: "2021",
+    },
+    {
+        jobtitle: "Internship",
+        where: "Pie Infocomm, Bengaluru",
+        date: "2023",
+    },
+    
+];
+
+const skills = [{
+        name: "Linux",
+        value: 90,
+    },
+    {
+        name: "AWS",
+        value: 85,
+    },
+    {
+        name: "DevOps",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Java",
+        value: 75,
+    },
+    {
+        name: "Android Studio",
+        value: 55,
+    },
+    {
+        name: "HTML & CSS",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Raspberry PI as NAS Server",
+    description: "This project aims to address these challenges by designing and implementing a NAS server on a Raspberry Pi that is low-cost, energy-efficient, easy to set up, and reliable. The server will be able to store and share files over a network, support multiple concurrent users, and provide reliable performance along with a failsafe sceondary drive where data will be synced from primary drive in the given internal. This project will allow more users to take advantage from the benefits of a NAS server, without having to spend a lot of money or invest a lot of time in setting it up. ",
+    },
+    {
+        title: "Cipher Cloud",
+        description: "Cipher Cloud aims to address this challenge by designing and implementing a dual server AE with KWS scheme that distributes the workload between two servers. This will provide increased performance for keyword search and reduce reliance on a single server with a better security. ",
+    },
+    {
+        title: "Voice of Citizen",
+        description: "A citizen uses a mobile app to raise complaints to the panchayath. The app allows for direct photo of the problem and accurate location, reducing disadvantages of traditional method. panchayath will be having a web application where they can view the complaint and update the status.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "prajwalbelur.ec@gmail.com",
+    YOUR_FONE: "+917899574383",
+    description: "Bengaluru, India",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/PrajwalBelur",
+    facebook: "https://www.facebook.com/profile.php?id=100073969131342",
+    linkedin: "https://www.linkedin.com/in/prajwalbelur/",
+    twitter: "https://twitter.com/BelurPrajwal?t=FJ2fXh22cm1kokh41sW0Eg&s=09",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
